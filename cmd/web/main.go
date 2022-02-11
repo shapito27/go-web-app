2eadc8e23d572b740f90ff81537cc3a2caf82548
shapito27 <legionerust@yandex.ru>
2022-02-11 17:38:29 +0500

    style fixes;  add title to layout

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/shapito27/go-web-app/pkg/handlers"
 )
 
+//app listen this port
 const portNumber = ":8080"
 
 func main() {
+	//routes
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	
 	http.ListenAndServe(portNumber, nil)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,7 +1,6 @@
 package render
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
 	"path/filepath"
@@ -12,48 +11,40 @@ var functions = template.FuncMap{}
 
 // To render templates
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	templates, err := getTemplatesCache()
+	templates, err := getTemplatesList()
 	if err != nil {
 		fmt.Println("Error getting templates cache", err)
 	}
 
-	t, ok := templates[tmpl + ".page.tmpl"]
+	t, ok := templates[tmpl+".page.tmpl"]
 	if !ok {
 		fmt.Println("Error getting template from cache")
 	}
-	
-		buf := new(bytes.Buffer)
 
-		_ = t.Execute(buf, nil)
+	err = t.Execute(w, nil)
 
-		_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
 
-		if err != nil {
-			fmt.Println("Error writing template to cachbrowser", err)
-		}
-	
 }
 
-func getTemplatesCache() (map[string]*template.Template, error) {
-
+//collect all templates then merge them with layout
+func getTemplatesList() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
 		return myCache, err
 	}
-	//fmt.Println(" pages is", pages)
 
 	matches, err := filepath.Glob("./templates/*.layout.tmpl")
 	if err != nil {
 		return myCache, err
 	}
-	//fmt.Println(" matches is", matches)
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-
-		//fmt.Println("Current page is", name)
 
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 
